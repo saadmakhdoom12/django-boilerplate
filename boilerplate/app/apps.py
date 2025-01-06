0273d69d3a204ccb0e6f99711775ea5511ef22ea
Saad Ahmed <saadmakhdoom12@gmail.com>
2025-01-06 23:11:54 +0500

    inital changes

--- boilerplate/app/apps.py
+++ boilerplate/app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+class MyAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "boilerplate.app"
