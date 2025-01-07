480e803e5a3b1229f2d18458747313640afc098d
MuSaXheikh <musa.yousaf212@gmail.com>
2025-01-07 22:24:20 +0500

    login functions

--- boilerplate/app/views.py
+++ boilerplate/app/views.py
@@ -1,14 +1,16 @@
 """imports"""
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-from django.core.mail import send_mail
-from django.conf import settings
-from .serializers import SignupSerializer
-from django.contrib.auth import get_user_model
 from boilerplate.app.token import account_activation_token
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
+from .serializers import LoginSerializer, SignupSerializer
 
 User = get_user_model()
 
@@ -38,5 +40,29 @@ class SignupView(APIView):
                     )
                 },
                 status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(
+                username=serializer.data["username"],
+                password=serializer.data["password"],
+            )
+            if user:
+                refresh = RefreshToken.for_user(user)
+                return Response(
+                    {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            return Response(
+                {"error": "Invalid credentials."},
+                status=status.HTTP_401_UNAUTHORIZED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
